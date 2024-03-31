b21d6af9fcf243e062ed211b55f914a8b48c858f
SCSS77 <scastro@emagister.com>
2024-03-31 14:07:48 +0200

    Add localStorage, expires in one day

--- src/hooks/UseCharacters.ts
+++ src/hooks/UseCharacters.ts
@@ -1,14 +1,27 @@
 import getCharactersList from '@/services/getCharactersList'
 import { useEffect, useState } from 'react'
 
+import { setLocalStorageWithExpiry, getLocalStorageWithExpiry } from '@/utils/localStorage'
+
 export default function useCharacters (searchQuery) {
   const [charactersResults, setCharactersResults] = useState(null)
 
   useEffect(() => {
+    const cookieValue = getLocalStorageWithExpiry('marvelCharacters')
+    const cookieParser = cookieValue !== null && JSON.parse(cookieValue)
+
+    if (cookieValue !== null) {
+      setCharactersResults(JSON.parse(cookieParser.value))
+      return
+    }
+
     async function fetchData () {
       try {
         const data = await getCharactersList(searchQuery)
         setCharactersResults(data.data.results)
+
+        const dataParser = JSON.stringify(data.data.results)
+        setLocalStorageWithExpiry('marvelCharacters', dataParser, 1)
       } catch (error) {
         console.log(error)
       }
