504c15a525b0068e6d06d9948883821ad7ee115b
SCSS77 <scastro@emagister.com>
2024-04-04 14:29:25 +0200

    localStorage for characters page detail, expires in one day

--- src/hooks/useCharacterDetail.ts
+++ src/hooks/useCharacterDetail.ts
@@ -1,19 +1,31 @@
 import getCharacterDetail from '@/services/getCharacterDetail'
 import { useEffect, useState } from 'react'
 
+import { setLocalStorageWithExpiry, getLocalStorageWithExpiry } from '@/utils/localStorage'
+
 export default function useCharacterDetail (characterId: number) {
   const [characterDetail, setCharacterDetail] = useState(null)
 
   useEffect(() => {
+    const cookieMarvelCharacterDetail = getLocalStorageWithExpiry('marvelCharacterDetail')
+    const cookieParser = cookieMarvelCharacterDetail !== null && JSON.parse(cookieMarvelCharacterDetail)
+
+    if (cookieMarvelCharacterDetail !== null) {
+      setCharacterDetail(JSON.parse(cookieParser.value))
+      return
+    }
+
     async function fetchCharacterDetail () {
       try {
         const detail = await getCharacterDetail(characterId)
         setCharacterDetail(detail)
+
+        const dataParser = JSON.stringify(detail)
+        setLocalStorageWithExpiry('marvelCharacterDetail', dataParser, 1)
       } catch (error) {
         console.error('Error fetching character detail:', error)
       }
     }
-
     fetchCharacterDetail()
   }, [characterId])
 
