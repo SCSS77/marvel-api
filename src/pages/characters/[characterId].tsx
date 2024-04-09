77979a259029005b8b83995d8b39da42b2af7ec1
SCSS77 <scastro@emagister.com>
2024-04-09 19:39:55 +0200

    add comics to the characters detail page and styles

--- src/pages/characters/[characterId].tsx
+++ src/pages/characters/[characterId].tsx
@@ -1,18 +1,36 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import Header from '@/components/Header'
-import useCharacterDetail from '@/hooks/useCharacterDetail'
-import { useFavorites } from '@/hooks/useFavorites'
 import CharacterLayout from '@/pages/characters/layout'
+import Header from '@/components/Header'
 import Loader from '@/components/Loader'
+import getCharacterDetail from '@/services/getCharacterDetail'
+import { useFavorites } from '@/hooks/useFavorites'
 
 const CharacterDetailPage = () => {
   const router = useRouter()
   const { characterId } = router.query
-  const { characterDetail, loading } = useCharacterDetail(Number(characterId))
+  const [characterData, setCharacterData] = useState(null)
+  const [loading, setLoading] = useState(true)
   const [favorites] = useFavorites([])
+
+  useEffect(() => {
+    const fetchCharacterDetail = async () => {
+      try {
+        const data = await getCharacterDetail(Number(characterId))
+        setCharacterData(data)
+        setLoading(false)
+      } catch (error) {
+        console.error('Error fetching character detail:', error)
+        setLoading(false)
+      }
+    }
+
+    if (characterId) {
+      fetchCharacterDetail()
+    }
+  }, [characterId])
 
   if (loading) return <Loader />
 
@@ -20,32 +38,41 @@ const CharacterDetailPage = () => {
     <CharacterLayout>
       <Header favoritesCount={favorites.length} />
       <main className='character-detail-main'>
-        <section className='character-detail-top-card'>
-          {characterDetail
-            ? (
-              <div className='character-detail-top-card__content'>
-                <div className='character-detail-top-card__image-content'>
-                  <img
-                    src={`${characterDetail.thumbnail.path}.${characterDetail.thumbnail.extension}`}
-                    alt={characterDetail.name}
-                    height={320}
-                    width={320}
-                    className='character-detail-top-card__image'
-                  />
-                </div>
-                <div className='character-detail-top-card__info'>
-                  <h1 className='character-detail-top-card__title'>{characterDetail.name}</h1>
-                  <p className='character-detail-top-card__description'>
-                    {characterDetail.description || 'Without description.'}
-                  </p>
-                </div>
+        {characterData && (
+          <section className='character-detail-top-card'>
+            <div className='character-detail-top-card__content'>
+              <div className='character-detail-top-card__image-content'>
+                <img
+                  src={`${characterData.character.thumbnail.path}.${characterData.character.thumbnail.extension}`}
+                  alt={characterData.character.name}
+                  height={320}
+                  width={320}
+                  className='character-detail-top-card__image'
+                />
               </div>
-              )
-            : (
-              <div>No character details found.</div>
-              )}
+              <div className='character-detail-top-card__info'>
+                <h1 className='character-detail-top-card__title'>{characterData.character.name}</h1>
+                <p className='character-detail-top-card__description'>
+                  {characterData.character.description || 'Without description.'}
+                </p>
+              </div>
+            </div>
+          </section>
+        )}
+
+        <section className='character-detail-comics'>
+          <h2 className='character-detail-comics__title'>Comics</h2>
+          <div className='character-detail-comics__container'>
+            <ul className='character-detail-comics__list'>
+              {characterData && characterData.comics && characterData.comics.map(comic => (
+                <div key={comic.id} className='character-detail-comics__card'>
+                  <img className='character-detail-comics__card-image' src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} alt={comic.title} />
+                  <p className='character-detail-comics__card-title'>{comic.title}</p>
+                </div>
+              ))}
+            </ul>
+          </div>
         </section>
-        <section className='character-detail-container' />
       </main>
     </CharacterLayout>
   )
--- src/services/getCharacterDetail.ts
+++ src/services/getCharacterDetail.ts
@@ -1,14 +1,28 @@
 export default async function getCharacterDetail (characterId: number) {
-  const url = new URL(`https://gateway.marvel.com/v1/public/characters/${characterId}`)
-  url.searchParams.append('ts', '1000')
-  url.searchParams.append('apikey', 'ae5a97fbf2bfbe2bdffb46f3e4d7e4ff')
-  url.searchParams.append('hash', 'd00ea43188f57a602b0e181280e0746a')
+  const characterUrl = new URL(`https://gateway.marvel.com/v1/public/characters/${characterId}`)
+  characterUrl.searchParams.append('ts', '1000')
+  characterUrl.searchParams.append('apikey', 'ae5a97fbf2bfbe2bdffb46f3e4d7e4ff')
+  characterUrl.searchParams.append('hash', 'd00ea43188f57a602b0e181280e0746a')
 
-  const response = await fetch(url.toString())
-  if (!response.ok) {
-    throw new Error('Failed to fetch character detail')
+  const comicsUrl = new URL(`https://gateway.marvel.com/v1/public/characters/${characterId}/comics`)
+  comicsUrl.searchParams.append('ts', '1000')
+  comicsUrl.searchParams.append('apikey', 'ae5a97fbf2bfbe2bdffb46f3e4d7e4ff')
+  comicsUrl.searchParams.append('hash', 'd00ea43188f57a602b0e181280e0746a')
+
+  const [characterResponse, comicsResponse] = await Promise.all([
+    fetch(characterUrl.toString()),
+    fetch(comicsUrl.toString())
+  ])
+
+  if (!characterResponse.ok || !comicsResponse.ok) {
+    throw new Error('Failed to fetch character or comics detail')
   }
 
-  const data = await response.json()
-  return data.data.results[0]
+  const characterData = await characterResponse.json()
+  const comicsData = await comicsResponse.json()
+
+  return {
+    character: characterData.data.results[0],
+    comics: comicsData.data.results
+  }
 }
